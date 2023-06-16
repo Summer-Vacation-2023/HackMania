6e2055fba303898615bdc87ce9362bb770825146
Akash Roy <royaku6@gmail.com>
2023-06-16 11:23:34 +0530

    Frontend Updated

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="bg-white rounded-lg shadow-md p-6">
           <h2 className="text-xl font-semibold mb-2">Featured Issues</h2>
-          <p className="text-gray-500 mb-4">Check out some important and popular issues in the community.</p>
+          <p className="text-gray-500 mb-4">Check out some important and popular issues of the residents.</p>
           {/* Featured */}
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
